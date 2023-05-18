756c8b24e04de2fe190771b244e03e55824e975d
Dan G <daniel.w.gray1994@gmail.com>
2023-05-18 17:17:21 -0400

    completed password generator

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,6 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 //DATA
-var charAmount = prompt("Select a character count between 8 and 128: ");
-var includeUppercase = confirm("Do you want to include uppercase characters?");
-var includeLowercase = confirm("Do you want to include lowercase characters?");
-var includeNum = confirm("Do you want to include number characters?");
-var includeSymbol = confirm("Do you want to include symbol characters?");
 
 var uppercaseCharCodes = pushToArray(65, 90);
 var lowercaseCharCodes = pushToArray(97, 122);
@@ -19,19 +14,6 @@ var uppercaseLetters = uppercaseCharCodes.map(code => String.fromCharCode(code))
 var lowercaseLetters = lowercaseCharCodes.map(code => String.fromCharCode(code));
 var numbers = numCharCodes.map(code => String.fromCharCode(code));
 var symbols = symbolCharCodes.map(code => String.fromCharCode(code));
-
-
-console.log(uppercaseLetters);
-console.log(lowercaseLetters);
-console.log(numbers);
-console.log(symbols);
-
-console.log(charAmount)
-console.log(includeUppercase)
-console.log(includeLowercase)
-console.log(includeNum)
-console.log(includeSymbol)
-
 
 // FUNCTIONS
 function pushToArray(lowNum, highNum) {
@@ -44,37 +26,54 @@ function pushToArray(lowNum, highNum) {
 
 
 function generatePassword() {
+
+  var password = ""
+  
+  // how long
+  var charAmount = Number(prompt("Select a character count between 8 and 128: "));
+  // ask the user what characters to use
+  var includeUppercase = confirm("Do you want to include uppercase characters?");
+  var includeLowercase = confirm("Do you want to include lowercase characters?");
+  var includeNum = confirm("Do you want to include number characters?");
+  var includeSymbol = confirm("Do you want to include symbol characters?");
+
   passwordCharacters = [];
 
+  // put all the chosen characters in to a list
   if (includeUppercase) {
     passwordCharacters = passwordCharacters.concat(uppercaseLetters)
-    console.log(passwordCharacters)
   };
-  // if (includeLowercase) {
-  //   passwordCharacters = passwordCharacters.concat(lowercaseLetters)
-  // };
-  // if (includeNum) {
-  //   passwordCharacters = passwordCharacters.concat(numbers)
-  // };
-  // if (includeSymbol) {
-  //   passwordCharacters = passwordCharacters.concat(symbols)
-  // };
+  if (includeLowercase) {
+    passwordCharacters = passwordCharacters.concat(lowercaseLetters)
+  };
+  if (includeNum) {
+    passwordCharacters = passwordCharacters.concat(numbers)
+  };
+  if (includeSymbol) {
+    passwordCharacters = passwordCharacters.concat(symbols)
+  };
 
-  // console.log(passwordCharacters)
+  // add as many random characters from the the chosen list as the user told me to
+  //    for loop to iterate based on user length input
+  for (i = 0; i < charAmount; i++) {
+    // get a random letter from the chosen list
+    // put it in the password 
+    // random method to randomize character selection from master array
+    var randomIndex = Math.floor(Math.random() * passwordCharacters.length);
+    password = password + passwordCharacters[randomIndex]
+  }
+  return password;
 };
 
-generatePassword();
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// // function writePassword() {
-// //   var password = generatePassword();
-// //   var passwordText = document.querySelector("#password");
-
-// //   passwordText.value = password;
-
-// }
+  passwordText.value = password;
+}
 
 
 
 // Add event listener to generate button
 // EVENT LISTENER
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
