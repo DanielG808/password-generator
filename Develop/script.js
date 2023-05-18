1c30fb397952552f4372f5dc987c15d5f5eb0e84
Dan G <daniel.w.gray1994@gmail.com>
2023-05-17 21:05:09 -0400

    add a function 'pushToArray'; declare var for upper, lower, number, and special char codes

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+//DATA
+uppercaseCharCodes = pushToArray(65, 90)
+lowercaseCharCodes = pushToArray(97, 122)
+numCharCodes = pushToArray(48, 57)
+symbolCharCodes = pushToArray(33,47).concat(pushToArray(58,64))
+// FUNCTIONS
+function generatePassword() {
+  String.fromCharCode()
+}
+
+function pushToArray(lowNum, highNum) {
+  var array = []
+  for (var i = lowNum; i <= highNum; i++) {
+    array.push(i)
+  }
+  return array
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +29,8 @@ function writePassword() {
 
 }
 
+console.log(uppercaseCharCodes)
+
 // Add event listener to generate button
+// EVENT LISTENER
 generateBtn.addEventListener("click", writePassword);
